c7f828e1c4663d1af066b16744e754f34040c0db
debyam <deboryambenu@gmail.com>
2023-02-28 20:55:48 +0200

    create a dataset of emails

---
+++ app/src/main/java/com/example/dmail/data/DataSource.kt
@@ -0,0 +1,248 @@
+package com.example.dmail.data
+
+import com.example.dmail.model.Email
+
+object DataSource {
+    val emails: List<Email> = listOf(
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        ),
+        Email(
+            "Darla smith",
+            "Recommendation",
+            "[Name of employee] has been working at [company] for [X] years, and played a vital role in [achievement].\n" +
+                    "\n" +
+                    "During that time, they have always been dedicated to [company] and displayed great team spirit, which we’re happy to reward with this promotion.\n" +
+                    "\n" +
+                    "I would be grateful if you join me in congratulating [name of employee] on this exciting news.\n" +
+                    "\n" +
+                    "Regards,\n" +
+                    "[name & position]"
+        )
+    )
+}
--- app/src/main/java/com/example/dmail/model/Email.kt
+++ app/src/main/java/com/example/dmail/model/Email.kt
@@ -1,7 +1,6 @@
 package com.example.dmail.model
 
 data class Email(
-    val image: String,
     val senderName: String,
     val objectMail: String,
     val contentMessage: String
